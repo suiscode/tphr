356e644faf92bf4024d425aacb2133f85550f9cb
suiscode <tuuduu00@gmail.com>
2024-05-13 06:46:48 +0800

    cache

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -2,9 +2,14 @@ import React from "react";
 import { DataTable } from "./data-table";
 import { User, columns } from "./columns";
 import { getAllUser } from "@/lib/fetch";
+import { NextResponse } from "next/server";
 
-const AdminPage = async () => {
+const AdminPage = async (context: any) => {
+  const { res } = context;
+  res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
   const data = await getAllUser();
+  console.log(data, 'GET ALL USER');
+  
   return (
     <div className="w-full">
       <DataTable columns={columns} data={JSON.parse(JSON.stringify(data))} />
